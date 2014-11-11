daeaf5221c9dc8a32cab857103b4bfbbfae4028c
tily <tidnlyam@gmail.com>
2014-11-11 23:02:52 +0900

    fix bug (see #3)

--- models/paragraphe.rb
+++ models/paragraphe.rb
@@ -1,11 +1,11 @@
-class Paragraph
+class Paragraphe
 	include Mongoid::Document
 	include Mongoid::Timestamps
 	field :body, type: String
 	validates :body,
 		length: {maximum: 1000, message: "は 1000 文字以内で入力してください"},
-		presence: {message: "が入力されていません"},
-		uniqueness: {message: "の内容が前の段落と同じです"}
+		presence: {message: "が入力されていません"}
+		#uniqueness: {message: "の内容が前の段落と同じです"}
 	belongs_to :story
 	has_many :characters
 end
